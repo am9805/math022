c65fe06f1a83893e0856c318fa20f5af6f5c4566
auxlis <auxlis@auxlis-OptiPlex-780>
2017-03-08 20:51:18 -0500

    Agregué el pergamino.

--- LaRana/src/VistaR/PrincipalRana.java
+++ LaRana/src/VistaR/PrincipalRana.java
@@ -16,7 +16,7 @@ public class PrincipalRana extends javax.swing.JFrame {
      */
     public PrincipalRana() {
         initComponents();
-                jLabel8.setVisible(false);
+                Gif.setVisible(false);
 
     }
 
@@ -29,39 +29,39 @@ public class PrincipalRana extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
-        jLabel5 = new javax.swing.JLabel();
-        jLabel7 = new javax.swing.JLabel();
-        jLabel8 = new javax.swing.JLabel();
+        RanaxRocaL = new javax.swing.JLabel();
+        Roca3 = new javax.swing.JLabel();
+        Roca1 = new javax.swing.JLabel();
+        Roca2 = new javax.swing.JLabel();
+        Roca4 = new javax.swing.JLabel();
+        Gif = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
-        jLabel6 = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
+        Fondo = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/VistaR/Recursos/RanaxRoca.png"))); // NOI18N
-        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 540, 190, 160));
+        RanaxRocaL.setIcon(new javax.swing.ImageIcon(getClass().getResource("/VistaR/Recursos/RanaxRoca.png"))); // NOI18N
+        getContentPane().add(RanaxRocaL, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 540, 190, 160));
 
-        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/VistaR/Recursos/Rocka2.png"))); // NOI18N
-        jLabel3.setText("jLabel1");
-        getContentPane().add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(510, 620, 150, 80));
+        Roca3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/VistaR/Recursos/Rocka2.png"))); // NOI18N
+        Roca3.setText("jLabel1");
+        getContentPane().add(Roca3, new org.netbeans.lib.awtextra.AbsoluteConstraints(510, 620, 150, 80));
 
-        jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/VistaR/Recursos/Rocka2.png"))); // NOI18N
-        jLabel4.setText("jLabel1");
-        getContentPane().add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 620, 150, 80));
+        Roca1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/VistaR/Recursos/Rocka2.png"))); // NOI18N
+        getContentPane().add(Roca1, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 620, 150, 80));
 
-        jLabel5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/VistaR/Recursos/Rocka2.png"))); // NOI18N
-        jLabel5.setText("jLabel1");
-        getContentPane().add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 620, 150, 80));
+        Roca2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/VistaR/Recursos/Rocka2.png"))); // NOI18N
+        Roca2.setText("jLabel1");
+        getContentPane().add(Roca2, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 620, 150, 80));
 
-        jLabel7.setIcon(new javax.swing.ImageIcon(getClass().getResource("/VistaR/Recursos/Rocka2.png"))); // NOI18N
-        jLabel7.setText("jLabel1");
-        getContentPane().add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(680, 620, 150, 80));
+        Roca4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/VistaR/Recursos/Rocka2.png"))); // NOI18N
+        Roca4.setText("jLabel1");
+        getContentPane().add(Roca4, new org.netbeans.lib.awtextra.AbsoluteConstraints(680, 620, 150, 80));
 
-        jLabel8.setIcon(new javax.swing.ImageIcon(getClass().getResource("/VistaR/Recursos/output_BnuDB1.gif"))); // NOI18N
-        getContentPane().add(jLabel8, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 410, 1000, 310));
+        Gif.setIcon(new javax.swing.ImageIcon(getClass().getResource("/VistaR/Recursos/output_BnuDB1.gif"))); // NOI18N
+        getContentPane().add(Gif, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 410, 1000, 310));
 
         jButton1.setText("jButton1");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
@@ -69,21 +69,26 @@ public class PrincipalRana extends javax.swing.JFrame {
                 jButton1ActionPerformed(evt);
             }
         });
-        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(850, 520, -1, -1));
+        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(860, 530, -1, -1));
 
-        jLabel6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/VistaR/Recursos/Fondo.jpg"))); // NOI18N
-        getContentPane().add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1000, 740));
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/VistaR/Recursos/Letrero.png"))); // NOI18N
+        jLabel1.setToolTipText("");
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(690, 170, 290, 350));
+
+        Fondo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/VistaR/Recursos/Fondo.jpg"))); // NOI18N
+        getContentPane().add(Fondo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1000, 740));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        jLabel8.setVisible(true);
-        jLabel2.setVisible(false);
-        jLabel3.setVisible(false);
-        jLabel4.setVisible(false);
-        jLabel5.setVisible(false);
-        jLabel7.setVisible(false);
+        Gif.setVisible(true);
+        RanaxRocaL.setVisible(false);
+        Roca3.setVisible(false);
+        Roca1.setVisible(false);
+        Roca2.setVisible(false);
+        Roca4.setVisible(false);
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
@@ -122,13 +127,14 @@ public class PrincipalRana extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel Fondo;
+    private javax.swing.JLabel Gif;
+    private javax.swing.JLabel RanaxRocaL;
+    private javax.swing.JLabel Roca1;
+    private javax.swing.JLabel Roca2;
+    private javax.swing.JLabel Roca3;
+    private javax.swing.JLabel Roca4;
     private javax.swing.JButton jButton1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel6;
-    private javax.swing.JLabel jLabel7;
-    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
